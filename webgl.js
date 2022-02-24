cd2bc04ea8e53460e5ba489b9566334346abcad2
Federico Bustaffa <kiko.bustaffa@gmail.com>
2022-02-24 14:50:37 +0100

    gl

--- webgl.js
+++ webgl.js
@@ -9,54 +9,88 @@ function setupWebGL() {
 var positionAttribIndex = 0;
 
 function setupWhatToDraw() {
-	var positions = [
-		0.0, 0.0,
-		1.0, 0.0,
-		0.0, 1.0
+	
+	var data = [
+		// coordinate (x, y)	// colore (rgb)
+		
+		// primo triangolo
+		-1.0, -0.1, 			//1.0, 0.0, 0.0,
+		-1.0,  0.1, 			//1.0, 0.0, 0.0,
+		 1.0, -0.1, 			//0.0, 0.0, 1.0,
+
+		 // secondo triangolo
+		 1.0,  0.1, 			//1.0, 0.0, 0.0,
+		 1.0, -0.1, 			//0.0, 0.0, 1.0,
+		-1.0,  0.1 			//1.0, 0.0, 0.0
 	];
-	var typedPositions = new Float32Array(positions);
-	var positionsBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, positionsBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, typedPositions, gl.STATIC_DRAW);
+	
+	var typedData = new Float32Array(data);
+	var buffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+	gl.bufferData(gl.ARRAY_BUFFER, typedData, gl.STATIC_DRAW);
 	gl.enableVertexAttribArray(positionAttribIndex);
-	gl.vertexAttribPointer(positionAttribIndex, 2, gl.FLOAT, false, 8, 0);
+	gl.vertexAttribPointer(positionAttribIndex, 2, gl.FLOAT, false, 2 * Float32Array.BYTES_PER_ELEMENT, 0);
 }
 
 function setupHowToDraw() {
-	var vs_source =
+	var vsSource =
 		`attribute vec2 aPosition;
-		
-		void main(void)
+		varying vec2 vPosition;
+
+		void main()
 		{
+			vPosition = aPosition;
 			gl_Position = vec4(aPosition, 0.0, 1.0);
 		}`;
-	var vertex_shader = gl.createShader(gl.VERTEX_SHADER);
-	gl.shaderSource(vertex_shader, vs_source);
-	gl.compileShader(vertex_shader);
 
-	var fs_source =
-		`precision lowp float;
+	var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+	gl.shaderSource(vertexShader, vsSource);
+	gl.compileShader(vertexShader);
+
+	var fsSource =
+		`precision highp float;
 		
-		void main(void)
+		varying vec2 vPosition;
+		vec3 color;
+		
+		void main()
 		{
-			gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+			if(vPosition.x < 0.0)
+			{
+				color.x = -vPosition.x;
+				color.y = 1.0 + vPosition.x;
+				color.z = 0.0;
+			}
+			else if(vPosition.x > 0.0)
+			{
+				color.x = 0.0;
+				color.y = 1.0 - vPosition.x;
+				color.z = vPosition.x;
+			}
+			else
+			{
+				color = vec3(0.0, 1.0, 0.0);
+			}
+			
+			gl_FragColor = vec4(color, 1.0);
 		}`;
-	var fragment_shader = gl.createShader(gl.FRAGMENT_SHADER);
-	gl.shaderSource(fragment_shader, fs_source);
-	gl.compileShader(fragment_shader);
+
+	var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+	gl.shaderSource(fragmentShader, fsSource);
+	gl.compileShader(fragmentShader);
 
 	var program = gl.createProgram();
-	gl.attachShader(program, vertex_shader);
-	gl.attachShader(program, fragment_shader);
+	gl.attachShader(program, vertexShader);
+	gl.attachShader(program, fragmentShader);
 	gl.bindAttribLocation(program, positionAttribIndex, "aPosition");
 	gl.linkProgram(program);
 	gl.useProgram(program);
 }
 
 function draw() {
-	gl.clearColor(0.0, 0.0, 0.0, 1.0);
+	gl.clearColor(0.2, 0.2, 0.2, 1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT);
-	gl.drawArrays(gl.TRIANGLES, 0, 3);
+	gl.drawArrays(gl.TRIANGLES, 0, 6);
 }
 
 function hello_draw() {
