4f53ce1590d8c09e92b5ae3d79755b2fbd1023cd
Federico Bustaffa <kiko.bustaffa@gmail.com>
2022-03-19 15:54:34 +0100

    cube

--- main.js
+++ main.js
@@ -74,15 +74,68 @@ function run() {
 
 	// GEOMETRY ----------------------------------------------------------
 	var vertices = new Float32Array([
-		-0.5, -0.5, 0.0,
-		0.0, 0.5, 0.0,
-		0.5, -0.5, 0.0
+		// FRONT
+		-0.5, -0.5, -0.5,
+		-0.5, 0.5, -0.5,
+		0.5, 0.5, -0.5,
+		0.5, -0.5, -0.5,
+
+		// TOP
+		-0.5, 0.5, -0.5,
+		-0.5, 0.5, 0.5,
+		0.5, 0.5, 0.5,
+		0.5, 0.5, -0.5,
+
+		// LEFT
+		-0.5, -0.5, -0.5,
+		-0.5, 0.5, -0.5,
+		-0.5, 0.5, 0.5,
+		-0.5, -0.5, 0.5,
+
+		// BACK
+		-0.5, -0.5, 0.5,
+		-0.5, 0.5, 0.5,
+		0.5, 0.5, 0.5,
+		0.5, -0.5, 0.5,
+
+		// BOTTOM
+		-0.5, -0.5, -0.5,
+		-0.5, -0.5, 0.5,
+		0.5, -0.5, 0.5,
+		0.5, -0.5, -0.5,
+
+		// RIGHT
+		0.5, -0.5, -0.5,
+		0.5, 0.5, -0.5,
+		0.5, 0.5, 0.5,
+		0.5, -0.5, 0.5
 	]);
 
 	var indices = new Uint16Array([
+		// FRONT
 		0, 1, 2,
+		2, 3, 0,
+
+		// BACK
+		4, 5, 6,
+		6, 7, 4,
+
+		// RIGHT
+		8, 9, 10,
+		10, 11, 8,
+
+		// LEFT
+		12, 13, 14,
+		14, 15, 12,
+
+		// TOP
+		16, 17, 18,
+		18, 19, 16,
+
+		// BOTTOM
+		20, 21, 22,
+		22, 23, 20
 	]);
-	var v = [1, 2, 3];
 
 	var verticesBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
@@ -98,9 +151,41 @@ function run() {
 
 	// COLOR
 	var color = new Float32Array([
+		// FRONT red
 		1.0, 0.0, 0.0,
+		1.0, 0.0, 0.0,
+		1.0, 0.0, 0.0,
+		1.0, 0.0, 0.0,
+
+		// BACK black
+		0.0, 0.0, 0.0,
+		0.0, 0.0, 0.0,
+		0.0, 0.0, 0.0,
+		0.0, 0.0, 0.0,
+
+		// RIGHT green
 		0.0, 1.0, 0.0,
-		0.0, 0.0, 1.0
+		0.0, 1.0, 0.0,
+		0.0, 1.0, 0.0,
+		0.0, 1.0, 0.0,
+
+		// LEFT blue
+		0.0, 0.0, 1.0,
+		0.0, 0.0, 1.0,
+		0.0, 0.0, 1.0,
+		0.0, 0.0, 1.0,
+
+		// TOP yellow
+		1.0, 1.0, 0.0,
+		1.0, 1.0, 0.0,
+		1.0, 1.0, 0.0,
+		1.0, 1.0, 0.0,
+
+		// BOTTOM white
+		1.0, 1.0, 1.0,
+		1.0, 1.0, 1.0,
+		1.0, 1.0, 1.0,
+		1.0, 1.0, 1.0
 	]);
 
 	var colorBuffer = gl.createBuffer();
@@ -113,13 +198,13 @@ function run() {
 
 	// TRANSFORMATIONS --------------------------------------------
 	var translationMatrix = glMatrix.mat4.create();
-	glMatrix.mat4.fromTranslation(translationMatrix, [0.5, 0.0, 0.0]);
+	glMatrix.mat4.fromTranslation(translationMatrix, [0.0, 0.0, 0.0]);
 
 	var scaleMatrix = glMatrix.mat4.create();
 	glMatrix.mat4.fromScaling(scaleMatrix, [1.0, 1.0, 1.0]);
 
 	var rotationMatrix = glMatrix.mat4.create();
-	glMatrix.mat4.fromRotation(rotationMatrix, Math.PI / 2, [0, 0, 1]);
+	glMatrix.mat4.fromRotation(rotationMatrix, 0.0, [0, 0, 0]);
 
 	var transformationMatrix = glMatrix.mat4.create();
 	glMatrix.mat4.mul(transformationMatrix, transformationMatrix, translationMatrix);
@@ -130,9 +215,20 @@ function run() {
 	gl.uniformMatrix4fv(transformationLocation, false, transformationMatrix);
 
 	// DRAW ----------------------------------------------------
-	gl.clearColor(0.2, 0.2, 0.2, 1.0);
-	gl.clear(gl.COLOR_BUFFER_BIT);
-	gl.drawElements(gl.TRIANGLES, 3, gl.UNSIGNED_SHORT, 0);
+	var angle = 0.01;
+	var loop = function () {
+		gl.enable(gl.DEPTH_TEST);
+
+		glMatrix.mat4.rotate(transformationMatrix, transformationMatrix, angle, [1, 1, 1]);
+		gl.uniformMatrix4fv(transformationLocation, false, transformationMatrix);
+
+		gl.clearColor(0.2, 0.2, 0.2, 1.0);
+		gl.clear(gl.COLOR_BUFFER_BIT);
+		gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
+
+		requestAnimationFrame(loop);
+	};
+	requestAnimationFrame(loop);
 }
 
 window.onload = run;
